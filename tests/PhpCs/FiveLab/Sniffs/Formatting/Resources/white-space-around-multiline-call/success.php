6e040ad8641192d62fe8f0b4b122e0a93daff433
Vitalii Zhuk <zhuk2205@gmail.com>
2022-06-02 11:17:51 +0300

    Fix some sniffs

--- tests/PhpCs/FiveLab/Sniffs/Formatting/Resources/white-space-around-multiline-call/success.php
+++ tests/PhpCs/FiveLab/Sniffs/Formatting/Resources/white-space-around-multiline-call/success.php
@@ -10,61 +10,58 @@ $object
         ))
         ->method('some');
 
-//$dom = new \DOMDocument();
-//
-//function some(\DOMDocument $dom): string
-//{
-//    $dom->loadXML(
-//        '<root/>',
-//        LIBXML_BIGLINES
-//    );
-//
-//    return \sprintf(
-//        'Success load xml %d',
-//        $dom->getLineNo()
-//    );
-//}
-//
-//$message = \sprintf('foo bar %s', 'some');
-//
-//$message = \sprintf(
-//    'foo %s',
-//    \sprintf(
-//        'bar %s',
-//        \sprintf(
-//            'some %d',
-//            1
-//        )
-//    )
-//);
-//
-//$bar = '0';
-//
-//$a = new SomeObject(
-//    new SomeActions([
-//        new SomeFooBar(
-//            new SomeObject()
-//        ),
-//    ])
-//);
-//
-//switch (true) {
-//    default:
-//        \sprintf(
-//            'foo bar',
-//            $b
-//        );
-//        break;
-//}
-//
-//#[FooBarAttribute()]
-//#[SomeAttribute([
-//    new Attribute1(),
-//    new Attribute2([
-//        1,
-//        2
-//    ]),
-//])]
-//function withAttributes()
-//{
-//}
+$dom = new \DOMDocument();
+
+function some(\DOMDocument $dom): string
+{
+    $dom->loadXML(
+        '<root/>',
+        LIBXML_BIGLINES
+    );
+
+    return \sprintf(
+        'Success load xml %d',
+        $dom->getLineNo()
+    );
+}
+
+$message = \sprintf('foo bar %s', 'some');
+
+$message = \sprintf(
+    'foo %s',
+    \sprintf(
+        'bar %s',
+        \sprintf(
+            'some %d',
+            1
+        )
+    )
+);
+
+$bar = '0';
+
+$a = new SomeObject(
+    new SomeActions([
+        new SomeFooBar(
+            new SomeObject()
+        ),
+    ])
+);
+
+switch (true) {
+    default:
+        \sprintf(
+            'foo bar',
+            $b
+        );
+        break;
+}
+
+#[Assert\NotBlank()]
+#[Assert\AtLeastOneOf([
+    new Assert\Type('string'),
+    new Assert\Type('numeric'),
+])]
+function withAttributes()
+{
+}
