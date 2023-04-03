d79264ea528ce2b924869b97c24fcfc83a6479af
maxvi <mr.maxvi@gmail.com>
2023-04-03 22:03:00 +0300

    Add semicolon single char sniff

---
+++ tests/PhpCs/FiveLab/Sniffs/Formatting/SemicolonSingleCharSniffTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the FiveLab CiRules package
+ *
+ * (c) FiveLab
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code
+ */
+
+declare(strict_types = 1);
+
+namespace FiveLab\Component\CiRules\Tests\PhpCs\FiveLab\Sniffs\Formatting;
+
+use FiveLab\Component\CiRules\PhpCs\FiveLab\Sniffs\Formatting\SemicolonSingleCharSniff;
+use FiveLab\Component\CiRules\Tests\PhpCs\FiveLab\Sniffs\SniffTestCase;
+
+class SemicolonSingleCharSniffTest extends SniffTestCase
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getSniffClass(): string
+    {
+        return SemicolonSingleCharSniff::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function provideDataSet(): array
+    {
+        return [
+            'success' => [
+                __DIR__.'/Resources/semicolon-single-char/success.php',
+            ],
+
+            'wrong' => [
+                __DIR__.'/Resources/semicolon-single-char/wrong.php',
+                [
+                    'message' => 'Close semicolon must be on one line with method/function call.',
+                    'source'  => 'FiveLab.Formatting.SemicolonSingleChar.WrongFormat',
+                ],
+            ],
+        ];
+    }
+}
