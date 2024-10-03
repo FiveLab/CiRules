85e20b527932e59eaef89c18c85e47529d74a8ab
maxvi <mr.maxvi@gmail.com>
2024-10-03 23:00:46 +0300

    Add checks white space around class property

--- src/PhpCs/FiveLab/ErrorCodes.php
+++ src/PhpCs/FiveLab/ErrorCodes.php
@@ -18,13 +18,15 @@ namespace FiveLab\Component\CiRules\PhpCs\FiveLab;
  */
 final class ErrorCodes
 {
-    public const MISSED_LINE_AFTER     = 'MissedLineAfter';
-    public const MISSED_LINE_BEFORE    = 'MissedLineBefore';
-    public const MISSED                = 'Missed';
-    public const WRONG_FORMAT          = 'WrongFormat';
-    public const MULTIPLE              = 'Multiple';
-    public const PROHIBITED            = 'Prohibited';
-    public const UNUSED                = 'Unused';
-    public const MISSED_PARAMETER_TYPE = 'MissedFunctionParameterType';
-    public const MISSED_RETURN_TYPE    = 'MissedFunctionReturnType';
+    public const MISSED_LINE_AFTER       = 'MissedLineAfter';
+    public const MISSED_LINE_BEFORE      = 'MissedLineBefore';
+    public const MISSED                  = 'Missed';
+    public const WRONG_FORMAT            = 'WrongFormat';
+    public const MULTIPLE                = 'Multiple';
+    public const PROHIBITED              = 'Prohibited';
+    public const UNUSED                  = 'Unused';
+    public const MISSED_PARAMETER_TYPE   = 'MissedFunctionParameterType';
+    public const MISSED_RETURN_TYPE      = 'MissedFunctionReturnType';
+    public const LINE_AFTER_NOT_ALLOWED  = 'LineAfterNotAllowed';
+    public const LINE_BEFORE_NOT_ALLOWED = 'LineBeforeNotAllowed';
 }
