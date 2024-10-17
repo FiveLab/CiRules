a0213e32af71183f9703986822e0accdca436924
maxvi <mr.maxvi@gmail.com>
2024-10-17 20:15:32 +0300

    Add ProhibitedInheritdocOnlySniff

---
+++ tests/PhpCs/FiveLab/Sniffs/Commenting/ProhibitedInheritdocOnlySniffTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the FiveLab CiRules package
+ *
+ * (c) FiveLab
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code
+ */
+
+declare(strict_types = 1);
+
+namespace FiveLab\Component\CiRules\Tests\PhpCs\FiveLab\Sniffs\Commenting;
+
+use FiveLab\Component\CiRules\PhpCs\FiveLab\Sniffs\Commenting\ProhibitedInheritdocOnlySniff;
+use FiveLab\Component\CiRules\Tests\PhpCs\FiveLab\Sniffs\SniffTestCase;
+
+class ProhibitedInheritdocOnlySniffTest extends SniffTestCase
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getSniffClass(): string
+    {
+        return ProhibitedInheritdocOnlySniff::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function provideDataSet(): array
+    {
+        return [
+            'success' => [
+                __DIR__.'/Resources/prohibited-inheritdoc-only/success.php',
+            ],
+
+            'wrong' => [
+                __DIR__.'/Resources/prohibited-inheritdoc-only/wrong.php',
+                [
+                    'message' => 'Prohibited {@inheritdoc} only comment.',
+                    'source'  => 'FiveLab.Commenting.ProhibitedInheritdocOnly.Prohibited',
+                ],
+            ],
+        ];
+    }
+}
---
+++ tests/PhpCs/FiveLab/Sniffs/Commenting/Resources/prohibited-inheritdoc-only/wrong.php
@@ -0,0 +1,11 @@
+<?php
+
+class WrongSome
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function foo()
+    {
+    }
+}
